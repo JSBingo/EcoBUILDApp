294e10dd72dcb835c1225657b68e248ff8a4e133
Jacqueline <jrosestringham123@gmail.com>
2022-04-26 19:07:43 -0400

    Initial Commit

---
+++ EcoBUILD/EcoBUILDApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoBUILDApp.swift
+//  EcoBUILD
+//
+//  Created by Jacqueline Stringham on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct EcoBUILDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcoBUILDUITests/EcoBUILDUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EcoBUILDUITestsLaunchTests.swift
+//  EcoBUILDUITests
+//
+//  Created by Jacqueline Stringham on 4/26/22.
+//
+
+import XCTest
+
+class EcoBUILDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
