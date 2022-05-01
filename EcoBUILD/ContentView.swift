eec61a27147a8f2c381375e91e069dcc1eea57d2
JSBingo <87412860+JSBingo@users.noreply.github.com>
2022-05-01 00:44:25 -0400

    Update ContentView.swift

--- EcoBUILD/ContentView.swift
+++ EcoBUILD/ContentView.swift
@@ -1,16 +1,38 @@
 //
 //  ContentView.swift
-//  EcoBUILD
+//  EcoBuild app for animals
 //
-//  Created by Jacqueline Stringham on 4/26/22.
+//  Created by Jacqueline Stringham on 3/22/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        VStack{
+            
+            Image("Eco").resizable().aspectRatio(contentMode:.fit).frame(width: 75.0, height: 75.0)
+            Text("EcoBuild").foregroundColor(.purple).bold()
+            
+            Text("Welcome to EcoBuild! EcoBuild is an app spreading awareness to endangered and threatened species to rebuild our ecosystems. We create a safe space for nature lovers to document sightings of endangered species and see where they could find more.")
+                .multilineTextAlignment(.center)
+                .lineLimit(8)
+                .padding().font(.custom("small", size: 18)).fixedSize(horizontal: false, vertical: true)            .position(.init(x: 185, y: 65))
+            Link("See official sightings here!", destination: URL(string: "https://www.mass.gov/info-details/rare-species-viewer" )!).position(x:200 , y: 55.0)
+            NavigationLink {
+                SwiftUIView()
+            } label: {
+                Text("Lets Go!").foregroundColor(.purple)
+            }.position(x: 165, y: 65)
+            
+            Text("Please share your experience with us!").position(.init(x:155,y:14)).font(.custom("small", size: 16))
+           
+            Image("EcoBLogo").resizable().aspectRatio(contentMode:.fit).frame(width: 575.0, height: 275.0).position(.init(x:170,y:30));
+           
+        }
+        }
     }
 }
 
@@ -18,4 +40,13 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+   
 }
+
+
+
+
+
+
+
+
